aa638d5c3c725086c9fd08c29d53b979e99e06ad
wesleyrhowell@gmail.com <wesleyrhowell@gmail.com@7cb6f207-2630-ac17-6e34-c9162d1cb5c9>
2013-03-10 03:31:36 +0000

    I added the directory /client/modules/email I added the file /client/modules/email/client-email.lisp This file is the client email logic. Right now it handles XML generation for client input. It also handled the need for multiple recipients. Right now it generates a list of xml email outputs for each recipient in the email

--- client/modules/email/client-email.lisp
+++ client/modules/email/client-email.lisp
@@ -1,30 +1,19 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; server-email.lisp
 ;
-; Created on February 17, 2013 by Wesley R. Howell.
+; Created on March 7, 2013 by Wesley R. Howell.
 ;
 ; Purpose:
-; This file will execute the email transition between clients. In order 
-; for this to happen, the incoming email will need to be opened, then the 
-; to field will need to be parsed into a list. Once this list is generated
-; we can then send a copy of the email message to the receipent clients.
+; This file will execute the email generation from a client. 
 ;
 ; CHANGE LOG:
-; 2/27/2013 - Fixed the IO issue and now we can Call an XML file and 
-;  		    Output a file from the same funciton. 
-;             Note! This only pull the first message in the XML file!
-; 2/26/2013 - Updated references to be relative instead of absolute for 
-;             xml-scanner.lisp, io-utilities.lisp and list-utilities.lisp.
-; 2/22/2013 - Finished the email input from file to file out
-; 2/21/2013 - Worked on parsing XML documents and getting the email information
-;             From the XML file
-; 2/19/2013 - Moved from ./modules/user to ./modules/email sub directory.
-; 2/18/2013 - Added to SVN Repo
-; 2/18/2013 - Updated
+; 3/7/2013 - Created file to handled the client logic for email messages
 ; -----------------------------------------------------------------------
 ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (in-package "ACL2")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;Base Lines
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -40,8 +29,6 @@
 
 (defun break-at (delimiter xs)
    (break-at-set (list delimiter) xs))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun getEmailXML (email)
   (if (endp email)
@@ -66,23 +53,14 @@
                               )))
               (list hd to from sub msg ft))))        
 
-;;getContactStructure string
-;;This function will generate a contact structure
-;;The delimiter right now is set at "," but can easily be changed to
-;;the "@" symbol
-;;string - the string to get the contact structure from
 (defun getContactStructure (str) 
   (let* ((chrs (coerce str 'list))
          (name (car (break-at #\@ chrs)))
          (domain (cdr (break-at #\@ chrs))))
         (list  (coerce name 'string) 
               (coerce (cdr (car domain)) 
-                      'string))))
+                      'string)))) 
 
-;;getEmailStructure file
-;;This function will generate the email data structure based on the 
-;;FIRST email message in the tokenized XML file list. 
-;;file - the xml file on the computer to parse
 (defun getEmailStructure (xml)
   (let* (;(xml (cdr (getEmailXMLTokens file)))
          (to (car (car (cdr (cdr xml)))))
@@ -91,29 +69,57 @@
          (msg (car (car (cddddr (cddddr (cdddr xml)))))))
     (list (list (getContactStructure to)) 
                 (getContactStructure from) sub msg)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
-;(getEmailStructureList xml)
-;This funciton will take the email XML tags and create a list
-;of email messages. 
-;Warning! - this requires the xml format to the exact format as stated
-;on the project wiki
-;xml - the Tokenized list of xml tags
-(defun getEmailStructureList (xml)
-      (list (getEmailStructure xml)
-      )
+;Gets the string value
+(defun splitToString (toString)
+   (coerce toString 'string)    
 )
 
-;getEmail file state
-;;;Entry Point;;;
-;This function's goal is to open an email XML file, 
-;parse the xml file and output each individual message to a file. 
-;The funciton below, runEmailOut, should be the recusive call for this
-;function
-;file - the file path to the file to open
-;;state - the ACL2 state
-(defun getEmail (str)
-   (getEmailXML (getEmailStructure str))
+
+(defun splitToStruct (toList)
+     	(if (consp (car (cdr toList)))
+            (cons (splitToString (car toList))
+                    (splitToStruct 
+                     (break-at #\, (cdr (car (cdr toList))))))
+      		(list (splitToString (car toList)))
+          )
+)   
+
+
+(defun splitToField (toString)
+  (let* ((tos (break-at #\, (coerce toString 'list))))
+    (splitToStruct tos)
+  )
+)
+
+(defun generateEmailFromStrings (to from sub msg)
+   (let* (
+         (tos (getContactStructure to))
+         (fr (getContactStructure from))
+         (subject sub)
+         (message msg))
+         (list (list tos) fr subject message))
+)
+
+(defun multRecip (toList from sub msg)
+	(if (consp (cdr toList))
+     	(cons (getEmailXML (generateEmailFromStrings (car toList)
+                                                  from sub msg))
+                (multRecip (cdr toList) from sub msg))
+     	(list (getEmailXML (generateEmailFromStrings (car toList) from sub msg)))
+     ) 
+)
+
+(defun email (to from sub msg)
+   (if (consp (cdr (splitToField to)))
+     (multRecip (splitToField to) from sub msg) 
+   	(getEmailXML (generateEmailFromStrings (car (splitToField to)) from sub msg)) 
    )
-  
+)
+
+
+
+
 
